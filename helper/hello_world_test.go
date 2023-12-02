7691fb46a5548c9924211b0bae9d7415d17ca9f8
Aditya170700 <adityaric72@gmail.com>
2023-12-02 09:44:41 +0700

    menggagalkan unit test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldAditya(t *testing.T) {
 	result := HelloWorld("Aditya")
 
 	if result != "Hello Aditya" {
-		panic("Result is not Hello Aditya")
+		t.Error("Result must be '" + result + "'")
 	}
+
+	fmt.Println("DONE")
+}
+
+func TestHelloWorldRicki(t *testing.T) {
+	result := HelloWorld("Ricki")
+
+	if result != "Hello Ricki" {
+		t.Fatal("Result must be '" + result + "'")
+	}
+
+	fmt.Println("DONE")
 }
